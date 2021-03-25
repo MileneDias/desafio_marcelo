c2cad4a53ded4ab9f3eef8c7fa2300df7ce67ed2
mileneDias <milene_diasdasilva@hotmail.com>
2021-03-25 20:56:11 -0300

    commitTeste

--- src/app/controllers/CategoriaController.js
+++ src/app/controllers/CategoriaController.js
@@ -1,6 +1,8 @@
 import Categoria from '../models/Categoria';
 import Produto from '../models/Produto';
 
+//Olá
+
 class CategoriaController {
     async index(req, res) {
         const categorias = await Categoria.findAll({
